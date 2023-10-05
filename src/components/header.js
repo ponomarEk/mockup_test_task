97bcb03535c8d9a9defdf1ecf0e2d4fd03176a22
Oleksandr Ponomarenko <ponomarek91@gmail.com>
2023-10-05 19:51:13 +0300

    added custom hook useQueryParam

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,21 @@
 import * as React from "react";
 
 import * as styles from "../styles/header.module.css";
+import useQueryParam from "../hooks/useQueryParam";
 
-const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
-    <h2>{siteTitle}</h2>
-    <input type="search" placeholder="Search..." />
-  </header>
-);
+const Header = ({ siteTitle }) => {
+  const [search, setSearch] = useQueryParam("search", "");
+  return (
+    <header className={styles.header}>
+      <h2>{siteTitle}</h2>
+      <input
+        type="search"
+        placeholder="Search..."
+        value={search}
+        onChange={({ target: { value } }) => setSearch(value)}
+      />
+    </header>
+  );
+};
 
 export default Header;
