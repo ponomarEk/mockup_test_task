ca908690ecaff30864d9e5890342610d426b3ba1
Oleksandr Ponomarenko <ponomarek91@gmail.com>
2023-10-08 21:27:01 +0300

    final fixes

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,14 +1,16 @@
 import React from "react";
+
 import { STATUS, GENDERS } from "../../../constants";
-import SelectFilter from "../Filter/Filter";
+
+import { Filter } from "../Filter";
 
 import * as styles from "./Filters.module.css";
 
 const Filters = () => {
   return (
     <div className={styles.filter_settings}>
-      <SelectFilter filterType={GENDERS} />
-      <SelectFilter filterType={STATUS} />
+      <Filter filterType={GENDERS} />
+      <Filter filterType={STATUS} />
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Search from "../Search/Search";
+import { Search } from "../Search";
 
 import * as styles from "./Header.module.css";
 
